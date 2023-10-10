7743d205627ba511e359f4be00ba038dbb547159
AbuBakar <abubakarsohail.dev@gmail.com>
2023-10-10 18:55:09 +0500

    added unit tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,3 +1,8 @@
 import { describe, expect, it } from "vitest";
 
 describe("test", () => it("should pass", () => expect(true).toBe(true)));
+
+describe("promptlayer", () =>
+  it("should not depend on openai", async () => {
+    expect(() => require("promptlayer").default.OpenAI).toThrowError();
+  }));
