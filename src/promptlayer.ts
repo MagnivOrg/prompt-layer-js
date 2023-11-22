47f931684e0c0fd2ebe724d3a350b6802a07b5e6
AbuBakar <abubakarsohail.dev@gmail.com>
2023-11-23 02:38:32 +0500

    Update package and version numbers

--- src/promptlayer.ts
+++ src/promptlayer.ts
@@ -21,53 +21,57 @@ export const promptLayerBase = (
     },
     get: (target, prop, receiver) => {
       const value = target[prop];
-      if (typeof value !== "function" && typeof value !== "object")
-        return Reflect.get(target, prop, receiver);
-      if (prop === "post") return value;
-      const function_name = Reflect.get(target, "function_name");
-      Object.defineProperties(value, {
-        function_name: {
-          value: `${function_name}.${prop.toString()}`,
-          writable: true,
-        },
-        provider: {
-          value: Reflect.get(target, "provider"),
-        },
-      });
-      return new Proxy(value, handler);
-    },
-    apply: (target, thisArg, argArray) => {
-      const request_start_time = new Date().toISOString();
-      const function_name = Reflect.get(target, "function_name");
-      const provider_type = Reflect.get(target, "provider");
-      const return_pl_id = argArray[0]?.return_pl_id;
-      const pl_tags = argArray[0]?.pl_tags;
-      delete argArray[0]?.return_pl_id;
-      delete argArray[0]?.pl_tags;
-      const response = Reflect.apply(target, thisArg, argArray);
-      if (response instanceof Promise) {
-        return new Promise((resolve, reject) => {
-          response
-            .then(async (request_response) => {
-              const response = await promptlayerApiHandler({
-                api_key: getApiKey(),
-                provider_type,
-                function_name,
-                request_start_time,
-                request_end_time: new Date().toISOString(),
-                request_response,
-                kwargs: argArray[0],
-                return_pl_id,
-                tags: pl_tags,
-              });
-              resolve(response);
-            })
-            .catch((error) => {
-              reject(error);
-            });
+      const function_name = `${Reflect.get(
+        target,
+        "function_name"
+      )}.${prop.toString()}`;
+      if (typeof value === "object") {
+        Object.defineProperties(value, {
+          function_name: {
+            value: function_name,
+            writable: true,
+          },
+          provider: {
+            value: provider,
+          },
         });
+        return new Proxy(value, handler);
       }
-      return response;
+      if (typeof value === "function") {
+        return (...args: any[]) => {
+          const request_start_time = new Date().toISOString();
+          const provider_type = Reflect.get(target, "provider");
+          const return_pl_id = args[0]?.return_pl_id;
+          const pl_tags = args[0]?.pl_tags;
+          delete args[0]?.return_pl_id;
+          delete args[0]?.pl_tags;
+          const response = Reflect.apply(value, target, args);
+          if (response instanceof Promise) {
+            return new Promise((resolve, reject) => {
+              response
+                .then(async (request_response) => {
+                  const response = await promptlayerApiHandler({
+                    api_key: getApiKey(),
+                    provider_type,
+                    function_name,
+                    request_start_time,
+                    request_end_time: new Date().toISOString(),
+                    request_response,
+                    kwargs: args[0],
+                    return_pl_id,
+                    tags: pl_tags,
+                  });
+                  resolve(response);
+                })
+                .catch((error) => {
+                  reject(error);
+                });
+            });
+          }
+          return response;
+        };
+      }
+      return Reflect.get(target, prop, receiver);
     },
   };
   return new Proxy(llm, handler);
