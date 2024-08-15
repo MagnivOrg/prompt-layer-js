bfd2a671350f14b46d70f5f2244d55912f78f782
buckyroberts <buckyroberts@gmail.com>
2024-08-15 14:40:59 -0400

    Allow users to set custom attributes

--- src/span-wrapper.ts
+++ src/span-wrapper.ts
@@ -1,12 +1,18 @@
 import * as opentelemetry from '@opentelemetry/api';
 import { getTracer } from '@/tracing';
 
-export const wrapWithSpan = (functionName: string, func: Function) => {
+export const wrapWithSpan = (functionName: string, func: Function, attributes?: Record<string, any>) => {
   return async function (...args: any[]) {
     const tracer = getTracer();
 
     return tracer.startActiveSpan(functionName, async (span) => {
       try {
+        if (attributes) {
+          Object.entries(attributes).forEach(([key, value]) => {
+            span.setAttribute(key, value);
+          });
+        }
+
         span.setAttribute('function_input', JSON.stringify(args));
         const result = await func(...args);
         span.setAttribute('function_output', JSON.stringify(result));
