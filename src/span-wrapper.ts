7ea480f58a6e1f5f5ece14ab7a50c35503cf49b7
Bucky Roberts <buckyroberts@gmail.com>
2024-08-19 09:40:47 -0400

    Handle both sync and async functions (#84)

--- src/span-wrapper.ts
+++ src/span-wrapper.ts
@@ -2,10 +2,10 @@ import * as opentelemetry from '@opentelemetry/api';
 import { getTracer } from '@/tracing';
 
 export const wrapWithSpan = (functionName: string, func: Function, attributes?: Record<string, any>) => {
-  return async function (...args: any[]) {
+  return function (...args: any[]) {
     const tracer = getTracer();
 
-    return tracer.startActiveSpan(functionName, async (span) => {
+    const wrapperFunction = (span: opentelemetry.Span) => {
       try {
         if (attributes) {
           Object.entries(attributes).forEach(([key, value]) => {
@@ -14,20 +14,38 @@ export const wrapWithSpan = (functionName: string, func: Function, attributes?: 
         }
 
         span.setAttribute('function_input', JSON.stringify(args));
-        const result = await func(...args);
-        span.setAttribute('function_output', JSON.stringify(result));
-        span.setStatus({ code: opentelemetry.SpanStatusCode.OK });
-        return result;
+        const result = func(...args);
+
+        if (result instanceof Promise) {
+          return result.then((resolvedResult) => {
+            span.setAttribute('function_output', JSON.stringify(resolvedResult));
+            span.setStatus({ code: opentelemetry.SpanStatusCode.OK });
+            return resolvedResult;
+          }).catch((error) => {
+            handleError(span, error, args);
+            throw error;
+          }).finally(() => span.end());
+        } else {
+          span.setAttribute('function_output', JSON.stringify(result));
+          span.setStatus({ code: opentelemetry.SpanStatusCode.OK });
+          span.end();
+          return result;
+        }
       } catch (error) {
-        span.setAttribute('function_input', JSON.stringify(args));
-        span.setStatus({
-          code: opentelemetry.SpanStatusCode.ERROR,
-          message: error instanceof Error ? error.message : 'Unknown error',
-        });
+        handleError(span, error, args);
         throw error;
-      } finally {
-        span.end();
       }
-    });
+    };
+
+    return tracer.startActiveSpan(functionName, wrapperFunction);
   };
 };
+
+const handleError = (span: opentelemetry.Span, error: any, args: any[]) => {
+  span.setAttribute('function_input', JSON.stringify(args));
+  span.setStatus({
+    code: opentelemetry.SpanStatusCode.ERROR,
+    message: error instanceof Error ? error.message : 'Unknown error',
+  });
+  span.end();
+}
